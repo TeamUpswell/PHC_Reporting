466e7bca1e4d8fbe5d4b99d38f7f7801edc0d294
Drew Bernard <82413471+TeamUpswell@users.noreply.github.com>
2025-04-25 10:20:49 -0700

    Your commit message here

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "78f4f433d85a0eb5"; }
+/******/ 		__webpack_require__.h = function() { return "809c007faece8ba7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
