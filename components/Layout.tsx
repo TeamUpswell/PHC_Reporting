d75e13cc81536e9f4d6ca782b09cd8eeda341f0e
Drew Bernard <82413471+TeamUpswell@users.noreply.github.com>
2025-04-30 10:07:34 -0700

    nav bar

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,39 +1,15 @@
 import { ReactNode } from "react";
-import { useRouter } from "next/router";
-import { useAuth } from "../context/AuthContext";
 import Navbar from "./Navbar";
-import Image from "next/image"; // Add this import
-import { isPublicRoute } from "../utils/routes";
 
 type LayoutProps = {
   children: ReactNode;
-  showNavbar?: boolean;
 };
 
-export default function Layout({ children, showNavbar = true }: LayoutProps) {
-  const router = useRouter();
-  const { user } = useAuth();
-  const isPublicPage = isPublicRoute(router.pathname);
-
+export default function Layout({ children }: LayoutProps) {
   return (
-    <div className="flex flex-col min-h-screen">
-      {/* Only show Navbar if showNavbar prop is true AND it's not a public page AND user is authenticated */}
-      {showNavbar && !isPublicPage && user && <Navbar />}
-
-      {/* Logo container - don't show on change-password page */}
-      {router.pathname !== "/change-password" && (
-        <div className="flex justify-center py-4">
-          <Image
-            src="/images/vital_logo.png"
-            alt="VITAL Logo"
-            width={150}
-            height={50}
-            priority
-          />
-        </div>
-      )}
-
-      <main className="flex-grow">{children}</main>
-    </div>
+    <>
+      <Navbar />
+      <main>{children}</main>
+    </>
   );
 }
